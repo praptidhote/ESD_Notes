0c565b5a24fc931edba21f9f045e5099cab06ed9
Prapti Dhote <praptidhote6509@gmail.com>
2026-07-22 15:55:32 +0530

    notes

---
+++ RTOS_EXAM/EXAM_rtosLED/Core/Src/main.c
@@ -0,0 +1,785 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "queue.h"
+#include "semphr.h"
+#include <stdio.h>
+#include <string.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart2;
+
+/* Definitions for defaultTask */
+osThreadId_t defaultTaskHandle;
+const osThreadAttr_t defaultTask_attributes = {
+  .name = "defaultTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* USER CODE BEGIN PV */
+QueueHandle_t xTempQueue;
+QueueHandle_t xAvgQueue;
+SemaphoreHandle_t xUARTSemaphore;
+
+TaskHandle_t xTask5Handle;
+
+uint32_t TempArray[8]={30,50,35,31,50,33,40,1};
+
+uint32_t MaxTemp,MinTemp,AvgTemp;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+void StartDefaultTask(void *argument);
+
+/* USER CODE BEGIN PFP */
+void vLEDTask(void *pvParam);
+void vSenderTask(void *pvParam);
+void vProcessTask(void *pvParam);
+void vDisplayLEDTask(void *pvParam);
+void vUARTTask(void *pvParam);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void vLEDTask(void *pvParam)
+{
+    TickType_t xLastWakeTime=xTaskGetTickCount();
+
+    while(1)
+    {
+        HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_15);
+        vTaskDelayUntil(&xLastWakeTime,pdMS_TO_TICKS(1000));
+    }
+}
+
+void vSenderTask(void *pvParam)
+{
+    uint32_t *arr=(uint32_t *)pvParam;
+
+    while(1)
+    {
+        for(int i=0;i<8;i++)
+            xQueueSend(xTempQueue,&arr[i],portMAX_DELAY);
+
+        vTaskDelay(pdMS_TO_TICKS(8000));
+    }
+}
+
+void vProcessTask(void *pvParam)
+{
+    uint32_t value,sum;
+
+    while(1)
+    {
+        sum=0;
+
+        for(int i=0;i<8;i++)
+        {
+            xQueueReceive(xTempQueue,&value,portMAX_DELAY);
+
+            if(i==0)
+            {
+                MaxTemp=value;
+                MinTemp=value;
+            }
+
+            if(value>MaxTemp)
+                MaxTemp=value;
+
+            if(value<MinTemp)
+                MinTemp=value;
+
+            sum+=value;
+        }
+
+        AvgTemp=sum/8;
+
+        xQueueSend(xAvgQueue,&AvgTemp,portMAX_DELAY);
+    }
+}
+
+void vDisplayLEDTask(void *pvParam)
+{
+    uint32_t Avg;
+
+    while(1)
+    {
+        xQueueReceive(xAvgQueue,&Avg,portMAX_DELAY);
+
+        if(Avg>32)
+        {
+            HAL_GPIO_WritePin(GPIOD,
+            GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14,
+            GPIO_PIN_SET);
+        }
+        else
+        {
+            HAL_GPIO_WritePin(GPIOD,
+            GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14,
+            GPIO_PIN_RESET);
+        }
+    }
+}
+
+void vUARTTask(void *pvParam)
+{
+    char msg[100];
+
+    while(1)
+    {
+        xSemaphoreTake(xUARTSemaphore,portMAX_DELAY);
+
+        sprintf(msg,
+        "Max=%lu  Min=%lu  Avg=%lu\r\n",
+        MaxTemp,MinTemp,AvgTemp);
+
+        HAL_UART_Transmit(&huart2,
+        (uint8_t *)msg,
+        strlen(msg),
+        HAL_MAX_DELAY);
+
+        xSemaphoreGive(xUARTSemaphore);
+
+        vTaskDelay(pdMS_TO_TICKS(3000));
+    }
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Init scheduler */
+  osKernelInitialize();
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  xUARTSemaphore=xSemaphoreCreateBinary();
+  xSemaphoreGive(xUARTSemaphore);
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  xTempQueue=xQueueCreate(8,sizeof(uint32_t));
+  xAvgQueue=xQueueCreate(1,sizeof(uint32_t));
+
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of defaultTask */
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  xTaskCreate(vLEDTask,
+  "LED",
+  configMINIMAL_STACK_SIZE,
+  NULL,
+  1,
+  NULL);
+
+  xTaskCreate(vSenderTask,
+  "Sender",
+  configMINIMAL_STACK_SIZE,
+  TempArray,
+  4,
+  NULL);
+
+  xTaskCreate(vProcessTask,
+  "Process",
+  configMINIMAL_STACK_SIZE,
+  NULL,
+  3,
+  NULL);
+
+  xTaskCreate(vDisplayLEDTask,
+  "Display",
+  configMINIMAL_STACK_SIZE,
+  NULL,
+  2,
+  NULL);
+
+  xTaskCreate(vUARTTask,
+  "UART",
+  configMINIMAL_STACK_SIZE,
+  NULL,
+  1,
+  &xTask5Handle);
+
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 50;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PD12 PD13 PD14 PD15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void *argument)
+{
+  /* USER CODE BEGIN 5 */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END 5 */
+}
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM6 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM6)
+  {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///*
+//          create a following RTOS tasks:
+//          1.TASK 1 (Low Priority)
+//          -Blink the on-Board LED every 1000ms.
+//          2.TASK 2
+//          -Take an array of 8 temperature reading as the arguments.
+//          -Example:{30,28,35,31,29,33,36,32}
+//          -send each temperature value one-by-one to a queue.
+//          3.TASK 3
+//          -Receive all temperature values from the queue
+//          -calculate:
+//          -Maximum Temperature
+//          -Minimum Temperature
+//          -Average Temperature
+//          -Send the Average Temperature to another queue
+//
+//          4.TASK 4
+//          -Receive the Average Temperature.
+//          -if Average Temperature > 32 c, turn ON all three LEDs.
+//          -Otherwise, turn Off all LEDs
+//
+//          5.TASK 5
+//          -Display the Maximum Temperature, Minimum Temperature and Average
+//          Temperature   on UART every 3 seconds
+//
+//          REQUIREMENTS:-
+//          -Create separate FreeRTOS tasks for each functionality
+//          -Use Queue for inter-task communication
+//          -Use Binary Semaphore for UART access.
+//          -Assign appropriate task Priorities
+//
+//
+//          only give code which i need to past dont give cubemx code
+//          and give in one page
+//
+//
+//
+//
+//
+//
+//          /* USER CODE BEGIN Includes */
+//#include "queue.h"
+//#include "semphr.h"
+//#include <stdio.h>
+//#include <string.h>
+///* USER CODE END Includes */
+//
+//
+///* USER CODE BEGIN PV */
+//
+//QueueHandle_t xTempQueue;
+//QueueHandle_t xAvgQueue;
+//SemaphoreHandle_t xUARTSemaphore;
+//
+//TaskHandle_t xTask5Handle;
+//
+//uint32_t TempArray[8]={30,28,35,31,29,33,36,32};
+//
+//uint32_t MaxTemp,MinTemp,AvgTemp;
+//
+///* USER CODE END PV */
+//
+//
+///* USER CODE BEGIN PFP */
+//
+//void vLEDTask(void *pvParam);
+//void vSenderTask(void *pvParam);
+//void vProcessTask(void *pvParam);
+//void vDisplayLEDTask(void *pvParam);
+//void vUARTTask(void *pvParam);
+//
+///* USER CODE END PFP */
+//
+//
+///* USER CODE BEGIN 0 */
+//
+//void vLEDTask(void *pvParam)
+//{
+//    TickType_t xLastWakeTime=xTaskGetTickCount();
+//
+//    while(1)
+//    {
+//        HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_15);
+//        vTaskDelayUntil(&xLastWakeTime,pdMS_TO_TICKS(1000));
+//    }
+//}
+//
+//void vSenderTask(void *pvParam)
+//{
+//    uint32_t *arr=(uint32_t *)pvParam;
+//
+//    while(1)
+//    {
+//        for(int i=0;i<8;i++)
+//            xQueueSend(xTempQueue,&arr[i],portMAX_DELAY);
+//
+//        vTaskDelay(pdMS_TO_TICKS(8000));
+//    }
+//}
+//
+//void vProcessTask(void *pvParam)
+//{
+//    uint32_t value,sum;
+//
+//    while(1)
+//    {
+//        sum=0;
+//
+//        for(int i=0;i<8;i++)
+//        {
+//            xQueueReceive(xTempQueue,&value,portMAX_DELAY);
+//
+//            if(i==0)
+//            {
+//                MaxTemp=value;
+//                MinTemp=value;
+//            }
+//
+//            if(value>MaxTemp)
+//                MaxTemp=value;
+//
+//            if(value<MinTemp)
+//                MinTemp=value;
+//
+//            sum+=value;
+//        }
+//
+//        AvgTemp=sum/8;
+//
+//        xQueueSend(xAvgQueue,&AvgTemp,portMAX_DELAY);
+//    }
+//}
+//
+//void vDisplayLEDTask(void *pvParam)
+//{
+//    uint32_t Avg;
+//
+//    while(1)
+//    {
+//        xQueueReceive(xAvgQueue,&Avg,portMAX_DELAY);
+//
+//        if(Avg>32)
+//        {
+//            HAL_GPIO_WritePin(GPIOD,
+//            GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14,
+//            GPIO_PIN_SET);
+//        }
+//        else
+//        {
+//            HAL_GPIO_WritePin(GPIOD,
+//            GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14,
+//            GPIO_PIN_RESET);
+//        }
+//    }
+//}
+//
+//void vUARTTask(void *pvParam)
+//{
+//    char msg[100];
+//
+//    while(1)
+//    {
+//        xSemaphoreTake(xUARTSemaphore,portMAX_DELAY);
+//
+//        sprintf(msg,
+//        "Max=%lu  Min=%lu  Avg=%lu\r\n",
+//        MaxTemp,MinTemp,AvgTemp);
+//
+//        HAL_UART_Transmit(&huart2,
+//        (uint8_t *)msg,
+//        strlen(msg),
+//        HAL_MAX_DELAY);
+//
+//        xSemaphoreGive(xUARTSemaphore);
+//
+//        vTaskDelay(pdMS_TO_TICKS(3000));
+//    }
+//}
+//
+///* USER CODE END 0 */
+//
+//
+///* USER CODE BEGIN RTOS_SEMAPHORES */
+//
+//xUARTSemaphore=xSemaphoreCreateBinary();
+//xSemaphoreGive(xUARTSemaphore);
+//
+///* USER CODE END RTOS_SEMAPHORES */
+//
+//
+///* USER CODE BEGIN RTOS_QUEUES */
+//
+//xTempQueue=xQueueCreate(8,sizeof(uint32_t));
+//xAvgQueue=xQueueCreate(1,sizeof(uint32_t));
+//
+///* USER CODE END RTOS_QUEUES */
+//
+//
+///* USER CODE BEGIN RTOS_THREADS */
+//
+//xTaskCreate(vLEDTask,
+//"LED",
+//configMINIMAL_STACK_SIZE,
+//NULL,
+//1,
+//NULL);
+//
+//xTaskCreate(vSenderTask,
+//"Sender",
+//configMINIMAL_STACK_SIZE,
+//TempArray,
+//2,
+//NULL);
+//
+//xTaskCreate(vProcessTask,
+//"Process",
+//configMINIMAL_STACK_SIZE,
+//NULL,
+//3,
+//NULL);
+//
+//xTaskCreate(vDisplayLEDTask,
+//"Display",
+//configMINIMAL_STACK_SIZE,
+//NULL,
+//2,
+//NULL);
+//
+//xTaskCreate(vUARTTask,
+//"UART",
+//configMINIMAL_STACK_SIZE,
+//NULL,
+//4,
+//&xTask5Handle);
+//
+///* USER CODE END RTOS_THREADS */
+
+
+
+
+
+
+
+
+
+
+               // */
